44a4e87952cedfa8b46481078d282a27e9530a08
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-31 17:43:07 +0000

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """ This is the main app """
 from flask import Flask, jsonify
-<<<<<<< HEAD
 from api.v1.views import app_views
-from os import getenv
-=======
->>>>>>> 7c5727d93da252d385ecb5af6ef5646a5a9879a7
-from models import storage
-from api.v1.views import app_views
-from os import getenv
+import os
 
 
 app = Flask('__name__')
@@ -17,13 +11,16 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(exception=None):
-    """
-        This is a teardown function
-    """
+    from models import storage
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+""" This is Basic routes """
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """
+        status route
+       :return: A response with json
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """
+        stats of all objects route
+        :return: A json representation of all objs
+    """
+    all_stats = {
+        "amenities": storage.close("Amenity"),
+        "cities": storage.close("City"),
+        "places": storage.close("Place"),
+        "reviews": storage.close("Review"),
+        "states": storage.close("State"),
+        "users": storage.close("User")
+    }
+
+    return jsonify(all_stats)
