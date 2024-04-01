97d26ae8050226df9011c2b8433cedc926afb72c
oswealth <osbertjaywealth@gmail.com>
2024-03-31 19:28:01 -0700

    Reviews

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,7 @@
 """ This is the Blueprint model """
 from flask import Blueprint
 
-
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
-
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -12,3 +10,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
