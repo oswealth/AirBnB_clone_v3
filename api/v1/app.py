5fe5c40e0a4d41a56a87cb5a243e592cc35f2a87
oswealth <osbertjaywealth@gmail.com>
2024-04-01 03:24:44 -0700

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """ This is the main app """
+
 from flask import Flask, jsonify
+from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+from flask_cors import CORS
 
-
-app = Flask('__name__')
+app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close(exception=None):
-    from models import storage
+def teardown_app(self):
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    """custom page not found msg"""
+    message = {"error": "Not found"}
+    return jsonify(message), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
